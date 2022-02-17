492e481cbc6b4a725b2d15a014b3b75ec9a19758
ACER\Z1402 <octaviaalya01@gmail.com>
2022-02-17 14:26:58 +0700

    arrow function 2

---
+++ Pertemuan 1 - Modern JavaScript/arrowf.js
@@ -0,0 +1,11 @@
+//tanpa arrow
+//const funct = function(a, b){
+// return a + b;
+//};
+//alert(funct(3,3));
+
+//Menggunakan arrow
+const func = (a, b) => {
+    return a + b;
+};
+alert(func(5,4));

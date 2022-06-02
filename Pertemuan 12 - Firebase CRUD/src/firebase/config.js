cf9a82ddf33f7ec1a1df098be9aefd52c68962f1
ACER\Z1402 <octaviaalya01@gmail.com>
2022-06-02 15:14:23 +0700

    upload code week 12

---
+++ Pertemuan 12 - Firebase CRUD/src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBCusdnF_6nnwwbnvFJ7B41Gr0BCH-xCOc",
+  authDomain: "prak12-118ba.firebaseapp.com",
+  projectId: "prak12-118ba",
+  storageBucket: "prak12-118ba.appspot.com",
+  messagingSenderId: "608940171785",
+  appId: "1:608940171785:web:97711c8df3f5579bf74ed8",
+  measurementId: "G-64BBPPY38E"
+  };
+  export default firebaseConfig;
